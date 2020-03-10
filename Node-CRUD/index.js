e6271842998e07ec859c892499ef2cb5b9431e97
SpencerOtoni <sspencerotoni@gmail.com>
2020-03-10 15:34:54 -0300

    commit

--- Node-CRUD/index.js
+++ Node-CRUD/index.js
@@ -3,6 +3,10 @@ const express = require('express')
 
 const server = express()
 
+server.get('/teste', (req,res) =>{
+    return res.json( { message: 'Hello world' })
+})
+
 server.listen(3000, () =>{
     console.log('Serdior Online.')
 })
